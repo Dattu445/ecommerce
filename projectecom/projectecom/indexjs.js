98fc38c7fcd9e4d6f7385b1453ef553a8102f017
dattu prasad <dattu prasad>
2017-03-15 13:37:56 -0500

    modified wit angular

--- projectecom/projectecom/indexjs.js
+++ projectecom/projectecom/indexjs.js
@@ -5,14 +5,25 @@ var applog = angular.module('myApp',['ngRoute']);
 
 applog.config([ '$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
-        $routeProvider.when('/Books', {
-            templateUrl: 'Books.html',
+        $routeProvider
+            .when('/Books', {
+            templateUrl: 'Templates/Books.html',
             controller: 'Bookscontroller'
-        })
+            })
+            .when('/Home', {
+                templateUrl: 'Templates/Home.html',
+                controller: 'Homecontroller'
+            })
+        .otherwise({
+            redirectTo : "/Home"
+        });
         
-        //$locationProvider.html5Mode(true); //Remove the '#' from URL.
+        $locationProvider.html5Mode(true); //Remove the '#' from URL.
     }
 ]);
 applog.controller("Bookscontroller", function ($scope) {
     $scope.message = "almost ochinatte chudam";
+});
+applog.controller("Homecontroller", function ($scope) {
+    $scope.message1 = "Homepage";
 });
